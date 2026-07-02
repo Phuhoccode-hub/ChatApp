ca4581cf6e88c40cfc630db031f43d92ef8b78a3
Phú Mai Hoàng Trương <phumht8112@ut.edu.vn>
2026-07-02 19:58:18 +0700

    Add files via upload

---
+++ server.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace SecureChatTCP_Server
+{
+    partial class server
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtServerLog = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txtServerLog
+            // 
+            this.txtServerLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtServerLog.Location = new System.Drawing.Point(0, 0);
+            this.txtServerLog.Name = "txtServerLog";
+            this.txtServerLog.Size = new System.Drawing.Size(800, 450);
+            this.txtServerLog.TabIndex = 0;
+            this.txtServerLog.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtServerLog);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtServerLog;
+    }
+}
+
